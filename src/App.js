77a587cbb6738c13a3a7f366b54a77e615a57f86
emilio sucre <emilio.sucre@utp.ac.pa>
2020-04-09 03:24:58 -0500

    se crea el primer componente funcional

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import WeatherLocation from './components/WeatherLocation';
 import logo from './logo.svg';
 import './App.css';
 
@@ -7,9 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <WeatherLocation/>
         <a
           className="App-link"
           href="https://reactjs.org"
