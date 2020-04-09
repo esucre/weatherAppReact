d6df5453087f4ffcbe5e23fda9f72319d3417b6b
emilio sucre <emilio.sucre@utp.ac.pa>
2020-04-09 04:01:57 -0500

    Se crean los componentes de la Locacion y el Weather Data con sus componentes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import React from 'react';
 import WeatherLocation from './components/WeatherLocation';
-import logo from './logo.svg';
+
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <WeatherLocation/>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
---
+++ src/components/WeatherTemperature.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const WeatherTemperature = () => (
+    <div><span>35°</span></div>
+);
+
+export default WeatherTemperature;
